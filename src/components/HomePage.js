5b789f0bbfd70ae99744b2f511182ad2578f367d
T-Frick <toby.frick@hotmail.com>
2023-04-10 22:14:26 +0200

    Small change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,16 +9,7 @@ import BlogView from './BlogView';
 
 
 const HomePage = (props) => {
-    const description = JSON.stringify({"blocks":[{"key":"904n","text":"THis is a sample description","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}})
-    const dummyPosts = [[1, {'Description' : description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [2, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [3, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [4, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [5, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [6, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [7, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [8, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}],
-                [9, {'Description': description, 'Title': 'Test Title', 'Date': '2021-10-10'}]]
+
     const [numDisplayed, setNumDisplayed] = useState(5)
     const [numPosts, setNumPosts] = useState(9)
     const [posts, setPosts] = useState(dummyPosts)
@@ -28,36 +19,10 @@ const HomePage = (props) => {
     const [clickedTitle, setClickedTitle] = useState('')
     const [clickedDate, setClickedDate] = useState('')
 
-    // Function to make  a post box
-    // const makePost = (description) => {
-    //     return <div className='entry'>{description}</div>
-    // }
-  // function to get all of the posts
   const getPosts = () => {
-    const auth = getAuth();
-    const user = auth.currentUser
-    if(user != null) {
-        const uid = user.uid
-        const q = query(collection(db, "UserPosts"), where("Author", "==", uid))
-        const tempPostData = []
-        getDocs(q).then((postData) => {
-            postData.forEach((doc) => {
-                tempPostData.push([doc.id, doc.data()])
-            })
-            const orderedPosts = orderPostsByDate(tempPostData)
-            setPosts(orderedPosts)
-            setNumPosts(orderedPosts.length)
-        })
-    }
-  }
-  const orderPostsByDate = (posts) => {
-    const orderedPosts = posts.sort((a, b) => {
-        const aDate = new Date(a[1]["Date"])
-        const bDate = new Date(b[1]["Date"])
-        return bDate - aDate
-    })
-    return orderedPosts
-  }
+
+
+        }
 
   const showMorePosts = () => {
     setNumDisplayed(numDisplayed + 16 > numPosts ? numPosts : numDisplayed + 16)
